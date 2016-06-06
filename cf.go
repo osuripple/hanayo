f0efb0233e300e6a6e0107f1e7050120dfaf3b24
Howl <the@howl.moe>
2016-06-06 21:36:39 +0200

    Add schiavo log, config, mysql

---
+++ cf.go
@@ -0,0 +1,6 @@
+package main
+
+// configuration
+type cf struct {
+	DSN string
+}
--- main.go
+++ main.go
@@ -1,13 +1,39 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 
+	"git.zxq.co/ripple/schiavolib"
 	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/thehowl/conf"
+)
+
+var (
+	c  cf
+	db *sql.DB
 )
 
 func main() {
 	fmt.Println("hanayo v0.0.1")
+
+	err := conf.Load(&c, "hanayo.conf")
+	switch err {
+	case nil:
+		// carry on
+	case conf.ErrNoFile:
+		conf.Export(c, "hanayo.conf")
+		fmt.Println("The configuration file was not found. We created one for you.")
+		return
+	default:
+		panic(err)
+	}
+
+	db, err = sql.Open("mysql", c.DSN)
+	if err != nil {
+		panic(err)
+	}
 
 	if gin.Mode() == gin.DebugMode {
 		fmt.Println("Development environment detected. Starting fsnotify on template folder...")
@@ -16,6 +42,8 @@ func main() {
 			fmt.Println(err)
 		}
 	}
+
+	schiavo.Bunker.Send(fmt.Sprintf("**hanayo** STARTUATO, mode: %s", gin.Mode()))
 
 	fmt.Println("Importing templates...")
 	loadTemplates()
