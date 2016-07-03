260cc9b15b5eecdac9311087895d9b28691d7d57
Howl <the@howl.moe>
2016-07-03 22:01:24 +0200

    Add initial sessions system

--- cf.go
+++
@@ -1,6 +0,0 @@
-package main
-
-// configuration
-type cf struct {
-	DSN string
-}
--- main.go
+++ main.go
@@ -5,13 +5,23 @@ import (
 	"fmt"
 
 	"git.zxq.co/ripple/schiavolib"
+	"git.zxq.co/x/rs"
+	"github.com/gin-gonic/contrib/sessions"
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/thehowl/conf"
 )
 
 var (
-	c  cf
+	c struct {
+		DSN                 string
+		CookieSecret        string
+		RedisEnable         bool
+		RedisMaxConnections int
+		RedisNetwork        string
+		RedisAddress        string
+		RedisPassword       string
+	}
 	db *sql.DB
 )
 
@@ -30,6 +40,10 @@ func main() {
 		panic(err)
 	}
 
+	if c.CookieSecret == "" {
+		c.CookieSecret = rs.String(46)
+	}
+
 	db, err = sql.Open("mysql", c.DSN)
 	if err != nil {
 		panic(err)
@@ -45,6 +59,24 @@ func main() {
 
 	schiavo.Bunker.Send(fmt.Sprintf("**hanayo** STARTUATO, mode: %s", gin.Mode()))
 
+	fmt.Println("Starting session system...")
+	var store sessions.Store
+	if c.RedisMaxConnections != 0 {
+		store, err = sessions.NewRedisStore(
+			c.RedisMaxConnections,
+			c.RedisNetwork,
+			c.RedisAddress,
+			c.RedisPassword,
+			[]byte(c.CookieSecret),
+		)
+		if err != nil {
+			fmt.Println(err)
+			store = sessions.NewCookieStore([]byte(c.CookieSecret))
+		}
+	} else {
+		store = sessions.NewCookieStore([]byte(c.CookieSecret))
+	}
+
 	fmt.Println("Importing templates...")
 	loadTemplates()
 
@@ -52,9 +84,13 @@ func main() {
 
 	r := gin.Default()
 
+	r.Use(sessions.Sessions("session", store))
+
 	r.Static("/static", "static")
 
 	r.GET("/", homePage)
+
+	conf.Export(c, "hanayo.conf")
 
 	r.Run(":45221")
 }
