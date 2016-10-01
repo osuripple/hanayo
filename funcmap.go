2f5eeed1cf462e62d377e0516acf7f00beb2b228
Howl <the@howl.moe>
2016-10-01 14:54:30 +0200

    show a warning when you're restricted

--- funcmap.go
+++ funcmap.go
@@ -147,8 +147,17 @@ var funcMap = template.FuncMap{
 	// log fmt.Printf's something
 	"log": fmt.Printf,
 	// has returns whether priv1 has all 1 bits of priv2, aka priv1 & priv2 == priv2
-	"has": func(priv1, priv2 float64) bool {
-		return uint64(priv1)&uint64(priv2) == uint64(priv2)
+	"has": func(priv1 interface{}, priv2 float64) bool {
+		var p1 uint64
+		switch priv1 := priv1.(type) {
+		case common.UserPrivileges:
+			p1 = uint64(priv1)
+		case float64:
+			p1 = uint64(priv1)
+		case int:
+			p1 = uint64(priv1)
+		}
+		return p1&uint64(priv2) == uint64(priv2)
 	},
 	// _range is like python range's.
 	// If it is given 1 argument, it returns a []int containing numbers from 0
