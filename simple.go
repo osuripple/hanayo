5b1c6271afddc873639c1f44a7c0907eff340eec
Howl <the@howl.moe>
2016-10-23 14:21:35 +0200

    Change settings header

--- simple.go
+++ simple.go
@@ -16,7 +16,7 @@ type simplePage struct {
 var simplePages = [...]simplePage{
 	{"/", "homepage.html", "Home Page", "homepage.jpg", 0},
 	{"/login", "login.html", "Log in", "login.png", 0},
-	{"/settings/avatar", "settings/avatar.html", "Change avatar", "settings.png", 2},
+	{"/settings/avatar", "settings/avatar.html", "Change avatar", "settings2.png", 2},
 	{"/dev/tokens", "dev/tokens.html", "Your API tokens", "dev.png", 2},
 	{"/beatmaps/rank_request", "beatmaps/rank_request.html", "Request beatmap ranking", "request_beatmap_ranking.jpg", 2},
 	{"/donate", "support.html", "Support Ripple", "donate2.png", 0},
