0d38f7f81d6a5bcca37b6a384257d7e29c9ee264
Morgan Bazalgette <the@howl.moe>
2016-12-08 15:20:07 +0100

    All JS now resides in dist.min.js
    This vastly improves load efficiency. dist.min.js gets generated with
    Gulp. the end file is of about 450kb, however with gzip it goes down to
    a little over 150kb. This makes a page like leaderboard load using
    545kb, excluding the header image. Of course, with caching this goes
    down to 7.2kb.
    
    With this commit, package.json is also added, containing the new
    dependencies. Also, vue has been added to the dependencies, because
    that's what we're gonna try implementing next.

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+var gulp    = require('gulp')
+var chug    = require('gulp-chug')
+var plumber = require('gulp-plumber')
+var uglify  = require('gulp-uglify')
+var flatten = require('gulp-flatten')
+var concat  = require('gulp-concat')
+var babel   = require('gulp-babel')
+
+gulp.task('default', function(){})
+gulp.task('build', [
+	"minify-js",
+])
+
+gulp.task('build-semantic', function() {
+	gulp.src("./semantic/gulpfile.js")
+		.pipe(chug())
+})
+
+gulp.task('minify-js', function() {
+	gulp
+		.src([
+			"static/licenseheader.js",
+			"node_modules/jquery/dist/jquery.min.js",
+			"node_modules/timeago/jquery.timeago.js",
+			"static/semantic.min.js",
+			"node_modules/vue/dist/vue.min.js",
+			"static/ripple.js",
+		])
+		.pipe(plumber())
+		.pipe(concat('dist.min.js'))
+		/*.pipe(babel({
+			presets: ["latest"]
+		})) breaks vue */
+		.pipe(flatten())
+		.pipe(uglify({
+			mangle: true,
+			preserveComments: "license"
+		}))
+		.pipe(gulp.dest("./static"))
+})
