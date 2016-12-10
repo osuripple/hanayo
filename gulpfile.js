968effd2c40e46f866081f8ee74b16f0b3e99add
Morgan Bazalgette <the@howl.moe>
2016-12-10 18:47:36 +0100

    Add gulp watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,29 @@
-var gulp    = require('gulp')
-var chug    = require('gulp-chug')
-var plumber = require('gulp-plumber')
-var uglify  = require('gulp-uglify')
-var flatten = require('gulp-flatten')
-var concat  = require('gulp-concat')
-var babel   = require('gulp-babel')
+var gulp    = require("gulp")
+var chug    = require("gulp-chug")
+var plumber = require("gulp-plumber")
+var uglify  = require("gulp-uglify")
+var flatten = require("gulp-flatten")
+var concat  = require("gulp-concat")
+var babel   = require("gulp-babel")
 
-gulp.task('default', function(){})
-gulp.task('build', [
+gulp.task("default", ["build"])
+gulp.task("build", [
 	"minify-js",
 ])
 
-gulp.task('build-semantic', function() {
-	gulp.src("./semantic/gulpfile.js")
-		.pipe(chug())
+gulp.task("watch", function() {
+	gulp.watch("static/*.js", ["minify-js"])
+	gulp.watch("semantic/src/**/*", ["build-semantic"])
 })
 
-gulp.task('minify-js', function() {
+gulp.task("build-semantic", function() {
+	gulp.src("./semantic/gulpfile.js")
+		.pipe(chug({
+			tasks: ['build']
+		}))
+})
+
+gulp.task("minify-js", function() {
 	gulp
 		.src([
 			"static/licenseheader.js",
@@ -28,7 +35,7 @@ gulp.task('minify-js', function() {
 			"static/ripple.js",
 		])
 		.pipe(plumber())
-		.pipe(concat('dist.min.js'))
+		.pipe(concat("dist.min.js"))
 		/*.pipe(babel({
 			presets: ["latest"]
 		})) breaks vue */
