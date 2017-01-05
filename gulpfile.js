27cd0848af0008e644350137804a7d242c241d7d
Morgan Bazalgette <the@howl.moe>
2017-01-05 22:19:24 +0100

    Add konami back

--- gulpfile.js
+++ gulpfile.js
@@ -29,6 +29,7 @@ gulp.task("minify-js", function() {
 			"static/licenseheader.js",
 			"node_modules/jquery/dist/jquery.min.js",
 			"node_modules/timeago/jquery.timeago.js",
+			"static/konami.js",
 			"static/semantic.min.js",
 			"node_modules/vue/dist/vue.min.js",
 			"static/ripple.js",
