ba96c4c693d32192ece6fc3454e8959dc1c7f3d9
Morgan Bazalgette <the@howl.moe>
2018-01-01 19:03:31 +0100

    redirect /help to support.ripple.moe

--- main.go
+++ main.go
@@ -315,6 +315,10 @@ func generateEngine() *gin.Engine {
 
 	r.Any("/blog/*url", blogRedirect)
 
+	r.GET("/help", func(c *gin.Context) {
+		c.Redirect(301, "https://support.ripple.moe")
+	})
+
 	loadSimplePages(r)
 
 	r.NoRoute(notFound)
