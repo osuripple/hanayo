a98499be01543fd81d3d8b8e6f141eaf079441ff
Morgan Bazalgette <the@howl.moe>
2018-02-04 12:00:10 +0100

    require license to be agreed before starting hanayo

--- main.go
+++ main.go
@@ -21,6 +21,7 @@ import (
 	"github.com/thehowl/qsql"
 	"gopkg.in/mailgun/mailgun-go.v1"
 	"gopkg.in/redis.v5"
+	"zxq.co/ripple/agplwarning"
 	"zxq.co/ripple/hanayo/modules/btcaddress"
 	"zxq.co/ripple/hanayo/modules/btcconversions"
 	"zxq.co/ripple/hanayo/routers/oauth"
@@ -97,9 +98,14 @@ var (
 )
 
 func main() {
+	err := agplwarning.Warn("ripple", "Hanayo")
+	if err != nil {
+		fmt.Println(err)
+	}
+
 	fmt.Println("hanayo " + version)
 
-	err := conf.Load(&config, "hanayo.conf")
+	err = conf.Load(&config, "hanayo.conf")
 	switch err {
 	case nil:
 		// carry on
